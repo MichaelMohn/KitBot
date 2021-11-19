e666fa9086cf6de9ab9db7942589ceda9b45afea
Michael <michael@mohn.com>
2021-11-18 21:37:00 -0600

    Kit Bot Day 1

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Talon;
+
+public class Drivetrain extends SubsystemBase {
+  Talon frontLeft = new Talon(2);
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void testMotor(double speed){
+    frontLeft.set(speed);
+  }
+}
